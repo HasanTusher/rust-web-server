2169d9e59af1399a54b6a0d855a39667acc1b534
HasanTusher <hasan.tusher787@gmail.com>
2024-06-24 21:09:37 +0600

    finally done

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,75 @@
 use std::thread;
+use std::thread::JoinHandle;
+use std::{sync::mpsc};
+use std::sync::{Arc, Mutex};
+
 pub struct ThreadPool{
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
 
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 impl ThreadPool{
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
 
-        let mut threads = Vec::with_capacity(size);
-
-        for _ in 0..size {
+        for id in 0..size {
             //create some threads and store them in the vector
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+            println!("{}", id);
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers, sender:Some(sender) }
     }
 
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+pub struct Worker{
+    id:usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker{
+     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread  = thread::spawn(move || loop {
+            let message  = receiver.lock().unwrap().recv();
+
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing");
+                    job();
+                },
+                Err(_) => {
+                    print!("Worker {id} disconnected; shutting down.");
+                }
+            }
+
+        });
+
+        Worker {id, thread: Some(thread)}
     }
 }
