b3d8c05029a2348d01ade5465d22885a3837c9c1
HasanTusher <hasan.tusher787@gmail.com>
2024-06-24 21:13:56 +0600

    guessing game done

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,18 @@
-use std::{
-    fs,
-    io::{prelude::*, BufRead},
-    net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
-};
+use std::io;
 use std::io::BufReader;
 use std::mem::forget;
 use hello::ThreadPool;
 
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
-    for stream in listener.incoming() {
-        let stream  = stream.unwrap();
+    println!("Guess the number");
 
-        pool.execute(|| {
-            handle_connection(stream);
-        })
-    }
+    println!("Please input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Error yay");
+
+    println!("you pressed {guess}");
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-
-    let (status_lilne, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-    };
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-
-    let response = format!("{status_lilne}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
-
-}
